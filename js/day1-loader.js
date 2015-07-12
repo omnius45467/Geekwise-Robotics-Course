e3238343830e3143abde572fc5aa8307afa2c5ae
sammyboy45467 <jeremy.robles56@gmail.com>
2015-07-12 00:33:56 -0700

    udpated content, need to write final example for day2

--- js/day1-loader.js
+++ js/day1-loader.js
@@ -1,3 +1,4 @@
-$( "#content" ).load( "ajax/day1-content.html", function() {
-    console.log( "Load day 1 content was performed." );
+$("#content").load("ajax/day1-content.html", function () {
+    console.log("Load day 1 content was performed.");
 });
+
---
+++ js/router.js
@@ -0,0 +1,15 @@
+var day1 = document.URL;
+switch(expression) {
+    case day2.html:
+        $("#content").load("ajax/day1-content.html", function () {
+            console.log("Load day 1 content was performed.");
+        });
+
+
+        break;
+    case n:
+        //code block
+        break;
+    default:
+
+}
